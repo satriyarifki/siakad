7323a4380df003c6aa5751651f385dc0ab5a3cef
satriyarp <rifkisatriya@gmail.com>
2022-03-28 09:36:39 +0700

    add model mahasiswa

---
+++ app/Models/Mahasiswa.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Foundation\Auth\Mahasiswa as Authenticatable;
+use Illuminate\Notifications\Notifiable;
+
+class Mahasiswa extends Model
+{
+    use HasFactory;
+    protected $table='mahasiswa';
+    protected $primaryKey='id_mahasiswa';
+    /**
+    * The attributes that are mass assignable.
+    *
+    * @var array
+    */
+    protected $fillable = [
+        'Nim',
+        'Nama',
+        'Kelas',
+        'Jurusan',
+        ];
+
+}
--- routes/web.php
+++ routes/web.php
@@ -22,4 +22,4 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
-Router::resource('mahasiswa', MahasiswaController::class);
+Route::resource('mahasiswa', MahasiswaController::class);
